49f9bf249438614a8abaddedb1d4a53904ccc54b
neittien0110 <neittien0110@gmail.com>
2019-03-11 20:33:16 +0700

    Tạo hàm gửi xung trig và đếm thời gian trả về trên chân echo

--- Ultrasonic.h
+++ Ultrasonic.h
@@ -5,6 +5,22 @@ const String HELP_ULTRASONIC = "   ";
 #define ECHOPIN 13
 #define TRIGPIN 12
 
+int UltrasonicSensorValue; //Giá trị thô lấy từ module siêu âm
+
+// @brief  Lây khoảng cách tới vật thể từ cảm biến siêu âm
+// @remark   nên đợi một chút, tầm 100ms trước khi gọi hàm lần nữa
+int get_distance(){
+  digitalWrite(TRIGPIN, LOW);                    // Set chan Trig xuong muc thap 2uS: ~~__
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN, HIGH);                   //Gui mot xung 10uS muc cao vao chan Trig ~~__----------
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);                    // Gui lai muc thap vao chan Trig         ~~__----------____
+  int distance = pulseIn(ECHOPIN, HIGH);        // Đếm thời gian (đơn vị us) cho tới khi chân ECHOPIN có sườn lên
+  distance= distance/58;                        // Qui đổi thời gian ra khoảng cách
+  return distance;
+}
+
+
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                                     SETUP
@@ -14,8 +30,6 @@ void setup_ultrasonic(){
   Serial.println(HELP_ULTRASONIC);
   pinMode(ECHOPIN, INPUT);
   pinMode(TRIGPIN, OUTPUT);
-  //pinMode(11, OUTPUT);
-  //pinMode(8,OUTPUT);
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -23,18 +37,6 @@ void setup_ultrasonic(){
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void loop_ultrasonic()
 {  
-  //digitalWrite(11,LOW);                          //chan gnd module
-  //digitalWrite(8,HIGH);                          // chan cap vcc module
-  //digitalWrite(TRIGPIN, LOW);                    // Set chan Trig xuong muc thap 2uS
-  //delayMicroseconds(2);
-  //digitalWrite(TRIGPIN, HIGH);                   //Gui mot xung 10uS muc cao vao chan Trig
-  //delayMicroseconds(10);
-  //digitalWrite(TRIGPIN, LOW);                    // Gui lai muc thap vao chan Trig
-  //int distance = pulseIn(ECHOPIN, HIGH);        // Doc thoi gian tra ve
-  //distance= distance/58;                        // Tinh toan khoang cach
-  //if (distance >= 0)
-  //{
-  //  Serial.println(distance);                    // Neu du lieu dung thi in ra
-  //  delay(100);
-  //}
+    UltrasonicSensorValue = get_distance();
+    Serial.println(UltrasonicSensorValue);                    // Hiển thị giá trị đo
 }
